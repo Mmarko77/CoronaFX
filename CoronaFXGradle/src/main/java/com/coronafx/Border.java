c02548f6884262571f9577cdc72e18deb655d560
AdrianVillin <4ndY252@gmail.com>
2020-06-01 19:35:33 +0200

    -opraveny dropdown + graf -pridane CSS na dropdown -zladenie farieb

--- CoronaFXGradle/src/main/java/com/coronafx/Border.java
+++ CoronaFXGradle/src/main/java/com/coronafx/Border.java
@@ -7,23 +7,23 @@ import java.util.ArrayList;
 
 public class Border {
     ArrayList<Integer> activeCases = new ArrayList<Integer>();
-    String [] countries = {"slovakia", "czechia", "poland", "ukraine", "hungary", "austria"};
-    public void getBorder(){
-        for (int i = 0; i < countries.length; i++) {
+    private String [] countries = {"slovakia", "czechia", "poland", "ukraine", "hungary", "austria"};
+    void getBorder(){
+        for (String country : countries) {
             URL url;
             String dataUrl;
             try {
-                dataUrl = "https://corona.lmao.ninja/v2/countries/" + countries[i];
+                dataUrl = "https://corona.lmao.ninja/v2/countries/" + country;
 
                 url = new URL(dataUrl);
                 URLConnection conn = url.openConnection();
                 BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
                 String inputLine;
-                String fileName = countries[i]+".json";
-                File file = new File("src/main/resources/data/"+fileName);
-                if (!file.exists()) {
+                String fileName = country + ".json";
+                File file = new File("src/main/resources/data/" + fileName);
+               /* if (!file.exists()) {
                     file.createNewFile();
-                }
+                }*/
 
                 FileWriter fw = new FileWriter(file.getAbsoluteFile());
                 BufferedWriter bw = new BufferedWriter(fw);
@@ -34,20 +34,37 @@ public class Border {
 
                 bw.close();
                 br.close();
-            } catch (IOException e){
+            } catch (IOException ignored) {
 
             }
         }
     }
 
-    public void splitBorder() throws IOException {
+    void splitBorder(String CBValue) throws IOException {
         String [] active;
-
-        for (int i = 0; i < countries.length; i++) {
-            File file = new File("src/main/resources/data/"+countries[i]+".json");
+        String [] activeDirty;
+        for (String country : countries) {
+            File file = new File("src/main/resources/data/" + country + ".json");
             BufferedReader reader = new BufferedReader(new FileReader(file));
             String data = reader.readLine();
-            String [] activeDirty = data.split("\"active\":");
+            switch (CBValue){
+                default:
+                    activeDirty = data.split("\"active\":");
+                    break;
+                case "\"cases\":":
+                    activeDirty = data.split("\"cases\":");
+                    break;
+                case "\"active\":":
+                    activeDirty = data.split("\"active\":");
+                    break;
+                case "\"deaths\":":
+                    activeDirty = data.split("\"deaths\":");
+                    break;
+                case "\"recovered\":":
+                    activeDirty = data.split("\"recovered\":");
+                    break;
+
+            }
             active = activeDirty[1].split(",");
             activeCases.add(Integer.parseInt(active[0]));
         }
--- CoronaFXGradle/src/main/java/com/coronafx/Controller.java
+++ CoronaFXGradle/src/main/java/com/coronafx/Controller.java
@@ -1,141 +1,204 @@
 package com.coronafx;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
 
+import java.text.DecimalFormat;
+
+import java.awt.event.ActionEvent;
 import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
-import java.text.DecimalFormat;
+import java.util.ResourceBundle;
 
-public class Controller {
-    @FXML Button getData;
-    @FXML TextField country;
-    @FXML Label totalLabel;
-    @FXML Label deathsLabel;
-    @FXML Label recoveredLabel;
-    @FXML Label activeLabel;
-    @FXML Label title;
-    @FXML AnchorPane background;
+public class Controller implements Initializable {
+    @FXML
+    Button getData;
+    @FXML
+    TextField country;
+    @FXML
+    Label totalLabel;
+    @FXML
+    Label deathsLabel;
+    @FXML
+    Label recoveredLabel;
+    @FXML
+    Label activeLabel;
+    @FXML
+    Label title;
+    @FXML
+    AnchorPane background;
 
-    @FXML BarChart<?, ?> graf;
-    @FXML CategoryAxis x;
-    @FXML NumberAxis y;
+    @FXML
+    BarChart<?, ?> graf;
+    @FXML
+    CategoryAxis x;
+    @FXML
+    NumberAxis y;
 
-    @FXML AnchorPane graphPane;
-    @FXML Button graphButton;
+    @FXML
+    AnchorPane graphPane;
+    @FXML
+    Button graphButton;
+    @FXML
+    ComboBox<String> comboBox;
 
 
- public void run() throws IOException {
+    public void run() {
 
-     background.setBackground(Background.EMPTY);
-     URL url;
-     String dataUrl;
+        background.setBackground(Background.EMPTY);
+        URL url;
+        String dataUrl;
 
-     try {
-         String inputText = country.getText();
-         if (country.getText().isEmpty()){
-             dataUrl = "https://corona.lmao.ninja/v2/all";
-         } else{
-             dataUrl = "https://corona.lmao.ninja/v2/countries/"+inputText;
-         }
+        try {
+            String inputText = country.getText();
+            if (country.getText().isEmpty()) {
+                dataUrl = "https://corona.lmao.ninja/v2/all";
+            } else {
+                dataUrl = "https://corona.lmao.ninja/v2/countries/" + inputText;
+            }
 
-         url = new URL(dataUrl);
-         URLConnection conn = url.openConnection();
-         BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-         String inputLine;
-         String fileName = "countries.json";
-         File file = new File("src/main/resources/data/"+fileName);
-         if (!file.exists()) {
-             file.createNewFile();
-         }
+            url = new URL(dataUrl);
+            URLConnection conn = url.openConnection();
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String inputLine;
+            String fileName = "countries.json";
+            File file = new File("src/main/resources/data/" + fileName);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
 
-         FileWriter fw = new FileWriter(file.getAbsoluteFile());
-         BufferedWriter bw = new BufferedWriter(fw);
+            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fw);
 
-         while ((inputLine = br.readLine()) != null) {
-             bw.write(inputLine);
-         }
+            while ((inputLine = br.readLine()) != null) {
+                bw.write(inputLine);
+            }
 
-         bw.close();
-         br.close();
-         splitData();
+            bw.close();
+            br.close();
+            splitData();
 
-     } catch (FileNotFoundException e) {
-         totalLabel.setText("Spolu: -");
-         deathsLabel.setText("Úmrtia: -");
-         recoveredLabel.setText("Zotavení: -");
-         activeLabel.setText("Nakazení: -");
-         country.setText("Krajina neexistuje");
+        } catch (FileNotFoundException e) {
+            totalLabel.setText("Spolu: -");
+            deathsLabel.setText("Úmrtia: -");
+            recoveredLabel.setText("Zotavení: -");
+            activeLabel.setText("Nakazení: -");
+            country.setText("Krajina neexistuje");
 
-     } catch (IOException ignored){
-     }
+        } catch (IOException ignored) {
+        }
 
- }
+    }
 
- private void splitData() throws IOException {
-     //DecimalFormat formatter = new DecimalFormat("###.###.###");
-     File file = new File("src/main/resources/data/countries.json");
-     BufferedReader reader = new BufferedReader(new FileReader(file));
+    private void splitData() throws IOException {
+        //DecimalFormat formatter = new DecimalFormat("###.###.###");
+        File file = new File("src/main/resources/data/countries.json");
+        BufferedReader reader = new BufferedReader(new FileReader(file));
 
-     String data = reader.readLine();
-     String [] totalDirty = data.split("\"cases\":");
-     String [] total = totalDirty[1].split(",");
-     //int totalInt = Integer.parseInt(total[0]);
-     totalLabel.setText("Spolu: "/*+formatter.format(totalInt)*/+total[0]);
+        String data = reader.readLine();
+        String[] totalDirty = data.split("\"cases\":");
+        String[] total = totalDirty[1].split(",");
+        //int totalInt = Integer.parseInt(total[0]);
+        totalLabel.setText("Spolu: "/*+formatter.format(totalInt)*/ + total[0]);
 
-     String [] deathsDirty = totalDirty[1].split("\"deaths\":");
-     String [] deaths = deathsDirty[1].split(",");
-     deathsLabel.setText("Úmrtia: "+deaths[0]);
+        String[] deathsDirty = totalDirty[1].split("\"deaths\":");
+        String[] deaths = deathsDirty[1].split(",");
+        deathsLabel.setText("Úmrtia: " + deaths[0]);
 
-     String [] recoveredDirty = deathsDirty[1].split("\"recovered\":");
-     String [] recovered = recoveredDirty[1].split(",");
-     recoveredLabel.setText("Zotavení: "+recovered[0]);
+        String[] recoveredDirty = deathsDirty[1].split("\"recovered\":");
+        String[] recovered = recoveredDirty[1].split(",");
+        recoveredLabel.setText("Zotavení: " + recovered[0]);
 
-     String [] activeDirty = recoveredDirty[1].split("\"active\":");
-     String [] active = activeDirty[1].split(",");
-     activeLabel.setText("Nakazení: "+active[0]);
- }
+        String[] activeDirty = recoveredDirty[1].split("\"active\":");
+        String[] active = activeDirty[1].split(",");
+        activeLabel.setText("Nakazení: " + active[0]);
+    }
 
- public void exit(){
-     System.exit(0);
- }
+    public void exit() {
+        System.exit(0);
+    }
 
- XYChart.Series set1 = new XYChart.Series<>();
- boolean clicked = false;
- public void graph() throws IOException {
-     graphPane.setVisible(true);
-     Border border = new Border();
-     border.getBorder();
-     border.splitBorder();
-     x.setAnimated(false);
+    private XYChart.Series set1 = new XYChart.Series<>();
 
-     if (!clicked) {
-         set1.getData().add(new XYChart.Data("Slovensko", border.activeCases.get(0)));
-         set1.getData().add(new XYChart.Data("Česko", border.activeCases.get(1)));
-         set1.getData().add(new XYChart.Data("Poľsko", border.activeCases.get(2)));
-         set1.getData().add(new XYChart.Data("Ukrajina", border.activeCases.get(3)));
-         set1.getData().add(new XYChart.Data("Maďarsko", border.activeCases.get(4)));
-         set1.getData().add(new XYChart.Data("Rakúsko", border.activeCases.get(5)));
-         graf.getData().addAll(set1);
-     }
+    public void graph() throws IOException {
+        graphPane.setVisible(true);
+
+        x.setAnimated(false);
         graphButton.setVisible(false);
         graf.setAnimated(false);
-        clicked = true;
+
+
     }
 
-public void hideGraph(){
-     graphPane.setVisible(false);
-     graf.setAnimated(true);
-     graphButton.setVisible(true);
+    public void hideGraph() {
+        graphPane.setVisible(false);
+        graf.setAnimated(true);
+        graphButton.setVisible(true);
     }
 
+    String CBValue = "";
+
+    public void parseCBValue() {
+
+        switch (comboBox.getValue()) {
+            case "Spolu":
+                CBValue = "\"cases\":";
+                break;
+            case "Úmrtia":
+                CBValue = "\"deaths\":";
+                break;
+            case "Zotavení":
+                CBValue = "\"recovered\":";
+                break;
+            case "Nakazení":
+                CBValue = "\"active\":";
+                break;
+        }
+        Border border = new Border();
+        border.getBorder();
+        try {
+            border.splitBorder(CBValue);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        set1.getData().clear();
+        set1.getData().add(new XYChart.Data("Slovensko", border.activeCases.get(0)));
+        set1.getData().add(new XYChart.Data("Česko", border.activeCases.get(1)));
+        set1.getData().add(new XYChart.Data("Poľsko", border.activeCases.get(2)));
+        set1.getData().add(new XYChart.Data("Ukrajina", border.activeCases.get(3)));
+        set1.getData().add(new XYChart.Data("Maďarsko", border.activeCases.get(4)));
+        set1.getData().add(new XYChart.Data("Rakúsko", border.activeCases.get(5)));
+        try {
+            graf.getData().addAll(set1);
+        } catch (IllegalArgumentException ignored){}
+
+
+
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        ObservableList<String> options = FXCollections.observableArrayList(
+                "Spolu",
+                "Úmrtia",
+                "Zotavení",
+                "Nakazení"
+        );
+        comboBox.getItems().addAll(options);
+    }
 }
